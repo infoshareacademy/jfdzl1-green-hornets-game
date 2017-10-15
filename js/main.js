2c229e89665102617520d745cc9f9a45291c571a
darqoo <d.smolag@gmail.com>
2017-10-15 13:57:16 +0200

    #1 create guy

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+var element = document.createElement('div');
+element.className = 'guy';
+
+function renderGuy () {
+    var body = document.getElementsByTagName('body')[0]
+    body.appendChild(element);
+}
+
+function moveGuy () {
+    
+}
+
+renderGuy();
+moveGuy();
