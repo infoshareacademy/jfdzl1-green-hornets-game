a41d256517ef3f979d82dbecf38b5c920f412684
Darqoo <d.smolag@gmail.com>
2017-10-17 18:04:54 +0200

    #2 adding guy who can move

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-
 var element = document.createElement('div');
-element.className = 'guy';
+var body = document.getElementsByTagName('body')[0];
+var guy = 0;
 
-function renderGuy () {
-    var body = document.getElementsByTagName('body')[0]
+function renderGuy() {
+    element.id = 'guy';
     body.appendChild(element);
+    guy = document.getElementById('guy');
+
 }
 
-function moveGuy () {
-    
+function moveGuy() {
+    body.addEventListener("keypress", function(event){
+        switch ( event.keyCode ) {
+            case 37: //w lewo
+                var left = document.getElementById('guy').offsetLeft;
+                if ( left === 0 ) {
+                    break;
+                }
+                guy.style.left = left - 10 + 'px';
+                break;
+            case 39: //w prawo
+                var resolutionWidth = window.innerWidth;
+                var left = document.getElementById('guy').offsetLeft;
+                if ( left >= resolutionWidth - 60 ) {
+                    break;
+                }
+                guy.style.left = left + 10 + 'px';
+                break;
+
+            default:
+                // empty
+        }
+    });
 }
 
 renderGuy();
