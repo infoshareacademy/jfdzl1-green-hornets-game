7d515631aea3ed39ff21b8e0feadb22f210328fc
darqoo <d.smolag@gmail.com>
2017-10-22 12:56:14 +0200

    score, lifes, catch the beer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
+var positionGuy;
+
+function collision(positionToCollision) {
+    if ((positionToCollision < positionGuy + 50) && (positionToCollision > positionGuy - 26)) {
+        scoreAdd();
+    } else {
+        lifesRemove();
+    }
+}
+
+function scoreAdd() {
+    var scoreTag = document.getElementById('score');
+    var score = parseInt(scoreTag.innerText);
+    score += 1;
+    scoreTag.innerText = score;
+}
+
+function lifesRemove() {
+    var lifesTag = document.getElementById('lifes');
+    var lifes = parseInt(lifesTag.innerText);
+    lifes -= 1;
+    lifesTag.innerText = lifes;
+    window.setTimeout(function () {
+        if (lifes === 0) {
+            alert("GAME OVER");
+        }
+    }, 200)
+}
+
 function initGuy() {
     var guyTag = document.createElement('div');
     guyTag.id = 'guy';
     document.body.appendChild(guyTag);
+    positionGuy = guyTag.offsetLeft;
 
     document.body.addEventListener('keydown', function (event) {
         var left = guyTag.offsetLeft;
@@ -9,10 +39,12 @@ function initGuy() {
             case 'ArrowLeft':
                 if (left > 0)
                     guyTag.style.left = left - 10 + 'px';
+                positionGuy = guyTag.offsetLeft;
                 break;
             case 'ArrowRight':
                 if (left < window.innerWidth - 60)
                     guyTag.style.left = left + 10 + 'px';
+                positionGuy = guyTag.offsetLeft;
                 break;
             default:
                 return;
@@ -25,7 +57,7 @@ function initBeer(speed) {
     speed = speed || 20;
 
     var beerTag = document.createElement('div');
-    beerTag.className = 'beer';    
+    beerTag.className = 'beer';
     document.body.appendChild(beerTag);
 
     var beerWidth = beerTag.offsetWidth;
@@ -38,21 +70,22 @@ function initBeer(speed) {
     var max = offsetLeftOfWinndow + (widthOfWinndow / 2) - (beerWidth / 2);
     var throwOutPoint = Math.floor(min + (Math.random() * (max - min)));
     beerTag.style.left = throwOutPoint + 'px';
-
-    animateBeer(beerTag, speed);
+    var positionToCollision = throwOutPoint;
+    animateBeer(beerTag, speed, positionToCollision);
 }
 
-function animateBeer(beerTag, speed) {
+function animateBeer(beerTag, speed, positionToCollision) {
     var position = beerTag.offsetTop;
     var inter = setInterval(function () {
         if (position >= window.innerHeight) {
             clearInterval(inter);
+            collision(positionToCollision);
         } else {
-            beerTag.style.top = (position += 3) + 'px';
+            beerTag.style.top = (position += 5) + 'px';
         }
     }, speed);
 }
 
-setInterval(initBeer, 500);
+setInterval(initBeer, 4000);
 
 initGuy();
