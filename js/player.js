f182dd293f23dd3dc5323bfd7ea772e1a960ac91
Krzysztof Durak <krzysztofdurak@gmail.com>
2017-11-08 10:56:09 +0100

    character animation working on keypress added background and files to conect as botle windows and grandma

--- js/player.js
+++ js/player.js
@@ -6,23 +6,27 @@ var canHeight = 300;
 
 var x = 0;
 var y = 0;
-var trackLeft = 1;
-var trackRight = 0;
+var trackLeft = 2;
+var trackRight = 1;
+var trackStand = 0;
 var left = false;
+var right = false;
+var stand = true;
 
 var scrX;
 var scrY;
 
 var sheetWidth = 720;
-var sheetHeight = 188;
+var sheetHeight = 300;
 
 var cols = 9;
-var rows = 2;
+var rows = 3;
+var speed = 12;
 
 var width = sheetWidth / cols;
 var height = sheetHeight / rows;
 var character = new Image();
-character.src = "./images/character2.png"
+character.src = "./images/character3.png"
 
 var currentFrame = 0;
 var canvas = document.getElementById('canvas');
@@ -33,10 +37,18 @@ var ctx  = canvas.getContext('2d');
 
 function moveRight(){
     left = false;
-
+    right = true;
+    stand = false;
 }
 function moveLeft(){
     left = true;
+    right = false;
+    stand = false;
+}
+function moveNone(){
+    left = false;
+    right = false;
+    stand = true;
 }
 
 function updateFrame(){
@@ -45,15 +57,34 @@ function updateFrame(){
 
     scrX = currentFrame * width;
 
-    if(left)
-        scrY = trackLeft * height;
-    else
-        x+=12;
-    scrY = trackRight * height;
+    if(stand)
+        scrY = trackStand * height;
+    else {
+        if (left)
+            scrY = trackLeft * height;
 
-//			srcY = 0;
+        else
+            scrY = trackRight * height;
+    }
+
+    console.log(stand, left, scrY)
 
 }
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+           moveLeft();
+            break;
+        case 39:
+            moveRight();
+            break;
+    }
+};
+document.onkeyup = function(){
+            moveNone();
+};
+
 function drawImage(){
     updateFrame();
     ctx.drawImage(character,scrX,scrY,width,height,x,y,width,height);
