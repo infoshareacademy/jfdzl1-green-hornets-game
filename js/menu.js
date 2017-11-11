cf7388b128be2e2736775d9f18aa4d271551e075
RadoslawStepniewski <radoslawstepniewski89@gmail.com>
2017-11-11 22:58:35 +0100

    added first event test on butttons

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+var buttonPlay = document.querySelector(".play");
+var buttonInstruction = document.querySelector(".instruction");
+
+buttonPlay.addEventListener("click", function () {
+    alert("hi");
+
+});
+buttonInstruction.addEventListener("click", function () {
+    alert("siema");
+
+});
+
+
